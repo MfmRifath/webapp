712e3c7c421d554980970b9621fdcdc47995c47d
Rifath MFM <mmfmrifath@gmail.com>
2025-05-08 12:18:13 +0530

    commited2a

--- src/test/java/com/S2O/webapp/WebappApplicationTests.java
+++ src/test/java/com/S2O/webapp/WebappApplicationTests.java
@@ -2,8 +2,10 @@ package com.S2O.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebappApplicationTests {
 
 	@Test
